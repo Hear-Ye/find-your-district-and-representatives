a19794f3912ffd056f7195bc3c902e272b7b7a33
Andrew-Chen-Wang <acwangpython@gmail.com>
2021-07-05 02:14:18 -0400

    Removed non-votable from tests (Fixes #2)

--- tests/test_geojson.py
+++ tests/test_geojson.py
@@ -18,12 +18,6 @@ from src.app import GEOJSON, get_federal_district
 
 
 votable = set(range(len(GEOJSON)))
-non_votable = {12, 87, 130, 231, 346, 417}
-# Get rid of non votable districts for testing sake
-# I'm going to add them back in once I figure out which
-# 6 VOTING districts are missing and what's causing the
-# geojson to fail.
-votable.difference_update(non_votable)
 
 
 @pytest.mark.parametrize("index", votable)
